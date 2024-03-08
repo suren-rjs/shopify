68c9cc5809e407e4e503dbcc006c85f535af66b6
Surendiran <suren.rjs@gmail.com>
2024-03-08 18:31:30 +0530

    Firest Commit

---
+++ src/redux/features/reviewApi.js
@@ -0,0 +1,20 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const reviewApi = apiSlice.injectEndpoints({
+  overrideExisting: true,
+  endpoints: (builder) => ({
+    addReview: builder.mutation({
+      query: (data) => ({
+        url: "https://shofy-backend-seven.vercel.app//api/review/add",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        "Products",
+        { type: "Product", id: arg.productId },
+      ],
+    }),
+  }),
+});
+
+export const { useAddReviewMutation } = reviewApi;
